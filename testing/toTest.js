19cd4a236fb2cc08fde753030f5800c826e719b0
Jesse <jtwolf@ucsd.edu>
2021-05-12 23:27:32 -0700

    Updated toTest.js to pass linting completely

--- testing/toTest.js
+++ testing/toTest.js
@@ -2,7 +2,7 @@
 
 // <journal-entry> custom web component
 class JournalEntry extends HTMLElement {
-  constructor() {
+  constructor () {
     super();
 
     // templated HTML content
@@ -52,16 +52,16 @@ class JournalEntry extends HTMLElement {
         `;
 
     // create a shadow root for this web component
-    this.attachShadow({ mode: 'open' })
-    // attach cloned content of template to shadow DOM 
-    this.shadowRoot.appendChild(template.content.cloneNode(true))
+    this.attachShadow({ mode: 'open' });
+    // attach cloned content of template to shadow DOM
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
   }
 
   /*
    * `get` binds a property to a function that will be called when that property is looked up
    * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/get
    */
-  get entry() {
+  get entry () {
     return this.getAttribute('entry');
   }
 
@@ -69,11 +69,11 @@ class JournalEntry extends HTMLElement {
    * `set` binds an object property to a function to be called when there is an attempt to set that property
    * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/set
    */
-  set entry(entry) {
-    /* 
+  set entry (entry) {
+    /*
      * TODO: set the entry title, date, and content fields in this component
      */
-    
+
     // CODE GOES HERE
 
     if (entry.image) {
@@ -87,20 +87,15 @@ class JournalEntry extends HTMLElement {
 
       // CODE GOES HERE vvv
 
-
-
-
-
       // CODE GOES HERE ^^^
 
       /* ------------- do not edit this code, it is for your debugging purposes ------------- */
       try {
         window.logCheckpoint('"entryImage"', imgExample, entryImage);
-      } catch(err) {
+      } catch (err) {
         console.log('variable name changed: ', err);
       }
       /* ------------- do not edit this code, it is for your debugging purposes ------------- */
-
     }
     if (entry.audio) {
       let entryAudio;
@@ -113,42 +108,33 @@ class JournalEntry extends HTMLElement {
 
       // CODE GOES HERE vvv
 
-
-
-
-
-
       // CODE GOES HERE ^^^
-      
 
-      
       /* ------------- do not edit this code, it is for your debugging purposes ------------- */
       try {
         window.logCheckpoint('"entryAudio"', exampleAudio, entryAudio);
-      } catch(err) {
+      } catch (err) {
         console.log('variable name changed: ', err);
       }
       /* ------------- do not edit this code, it is for your debugging purposes ------------- */
-
     }
     this.setAttribute('entry', entry);
 
     /* ------------- do not edit this code, it is for your debugging purposes ------------- */
     try {
       window.logCheckpoint('"entry"', exampleEntry, entry);
-    } catch(err) {
+    } catch (err) {
       console.log('variable name changed: ', err);
     }
     /* ------------- do not edit this code, it is for your debugging purposes ------------- */
   }
-
 }
 
 /*
- * Define a custom element for the JournalEntry web component, 
+ * Define a custom element for the JournalEntry web component,
  * where 'journal-entry' is the string that represents this element.
  * https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_custom_elements
- */ 
+ */
 customElements.define('journal-entry', JournalEntry);
 
 /**
@@ -167,12 +153,6 @@ customElements.define('journal-entry', JournalEntry);
  * }
  */
 
-
-
-
-
-
-
 /* ------------- do not edit this code, it is for your debugging purposes ------------- */
 const exampleEntry = {
   title: 'entry title',
@@ -180,20 +160,20 @@ const exampleEntry = {
   content: 'some sentences of entry content',
   audio: 'there may or may not be audio',
   image: 'there may or may not be image'
-}
+};
 const titleStyle = 'font-weight: bold; font-size: 16px; color: blue;';
 const contentStyle = 'font-weight: bold; font-size: 12px';
-const sectionBreak = '%c------------------------------------------------------------------------'
-const imgExample = '<img class="entry-image" src="path/to/image.jpg" alt="some alternate text"/>'
-const exampleAudio = '<audio class="entry-audio" src="https://url-for-source-audio.mp3" controls="">'
+const sectionBreak = '%c------------------------------------------------------------------------';
+const imgExample = '<img class="entry-image" src="path/to/image.jpg" alt="some alternate text"/>';
+const exampleAudio = '<audio class="entry-audio" src="https://url-for-source-audio.mp3" controls="">';
 
 window.logCheckpoint = (task, example, value) => {
   if (!window.logCheckpoints) return;
-  console.log(`%cTask ${task}: `, titleStyle)
-  console.log(`%cyour ${task} should look like this: `, contentStyle)
+  console.log(`%cTask ${task}: `, titleStyle);
+  console.log(`%cyour ${task} should look like this: `, contentStyle);
   console.log(example);
   console.log(`%cyour ${task}: `, contentStyle);
   console.log(value);
   console.log(sectionBreak, contentStyle);
-}
+};
 /* ------------- do not edit this code, it is for your debugging purposes ------------- */
