a4182707cd9e9cd09b265fad42fe0b3a5638689c
Jesse <jtwolf@ucsd.edu>
2021-05-30 18:44:17 -0700

    Added jest-puppeteer config that will spin up our server on port 8080 so that our pipeline will be able to find it.

---
+++ project/jest-puppeteer.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  launch: {
+    headless: false,
+    slowMo: false // Change this to false if you want it to be faster. Currently slows it down so that you can see what it's doing.
+  },
+  server: {
+    command: `npx http-server ./src`,
+    port: 8080,
+    launchTimeout: 10000,
+    debug: true
+  }
+};
