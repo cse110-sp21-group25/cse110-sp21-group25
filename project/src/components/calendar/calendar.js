6c9c45cfd715ddf676ba49e4c2c28a5c57d1e020
issacgarcia <isg001@ucsd.edu>
2021-06-01 22:27:09 -0700

    FullCalendarApi rendered on entry-container

--- project/src/components/calendar/calendar.js
+++ project/src/components/calendar/calendar.js
@@ -1,24 +1,27 @@
 /** Calendar Web Component */
 class Calendar extends HTMLElement {
-    constructor () {
-      super();
-  
-      const template = document.createElement('template');
-  
-      template.innerHTML = `
-        <link rel="stylesheet" href="./components/calendar/calendar.css">
-  
-        <div class="date-container">
-          <span class="date-text"></span>
+  constructor () {
+    super();
+
+    const template = document.createElement('template');
+
+    template.innerHTML = `
+    <link rel="stylesheet" href="./components/calendar/calendar.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fullcalendar@5.7.0/main.min.css"/>
+        <div id='calendar'>
+
         </div>
       `;
-  
-      this.attachShadow({ mode: 'open' });
-      this.shadowRoot.appendChild(template.content.cloneNode(true));
-  
-      
-    }
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
   }
-  
-  window.customElements.define('calendar', Calendar);
-  
+}
+document.addEventListener('DOMContentLoaded', function () {
+  const calendarEl = document.querySelector('my-calendar').shadowRoot.querySelector('#calendar');
+  const calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: 'dayGridMonth'
+  });
+  calendar.render();
+});
+
+window.customElements.define('my-calendar', Calendar);
