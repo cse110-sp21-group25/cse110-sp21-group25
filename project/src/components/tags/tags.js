9e3d3cb4e2d19e882c6550e50c95f4bb0f023d73
Angel <anh008@ucsd.edu>
2021-06-01 21:27:24 -0700

    Added basic color picker , will incorporate storing in array

--- project/src/components/tags/tags.js
+++ project/src/components/tags/tags.js
@@ -1,5 +1,5 @@
 
-const sample = ['la', 'li', 'lu', 'le', 'lol'];
+const sample = [['la', 'red'], ['li', 'blue'], ['lu', 'yellow'], ['le', 'green'], ['lo', 'silver']];
 class tagBujo extends HTMLElement {
   constructor () {
     super();
@@ -7,52 +7,42 @@ class tagBujo extends HTMLElement {
 
     template.innerHTML = `
     <style>
-      .tag {
-
+        .tag {
         border-color: none;
         color: black;
         padding-top: 0.125%;
         padding-right: 0.25%;
-        padding-bottom: 0.125%;
+        padding-bottom: 0.2%;
         padding-left: 1%;
-      
         text-align: center;
         text-decoration: none;
         display: inline-block;
-  
         font-size: 16px;
-        
-
-        background-color: silver;
-      }
+        margin-left = 100px;
+        }
+    
       
-        
-      button {
-
-        border: none;
-        color: black;
- 
-        border-radius: 10%;
-        text-align: center;
-        text-decoration: none;
-        display: inline-block;
-        font-size: 75%;
-        cursor: pointer;
-        background-color: silver;
-      }
-      
-      button:hover{
-        background-color: gray;
-      }
-
-      button:focus{
-          backroung-color: red;
-      }
-      </style>
-
+    button {
+      border: none;
+      color: black;
+      border-radius: 10%;
+      text-align: center;
+      text-decoration: none;
+      display: inline-block;
+      font-size: 65%;
+      cursor: pointer;
+      background-color: transparent;
+    }
+    
+    button:hover{
+      background-color: gray;
+    }
+    button:focus{
+        backroung-color: red;
+    }
+    </style>
       <div class="tag">
       <span></span>
-      
       <button id = deleter onclick='console.log(this.class);this.parentNode.remove();'>&#10005</button>
       </div>
       `;
@@ -62,6 +52,7 @@ class tagBujo extends HTMLElement {
 
   // TODO: Load tags from a JSON, or whatever method we use to store entires, we should create an array of tags, and each one will have content and background color
   // TODO: Add x mark that will delete the tag from the entry when clicked
+
   get tag () {
     return this.getAttribute('tag');
   }
@@ -70,10 +61,11 @@ class tagBujo extends HTMLElement {
     const currTag = this.shadowRoot;
     const text = currTag.querySelector('span');
     const tagid = currTag.querySelector('div');
-
-    tagid.setAttribute('id', tag[0]);
-
-    text.textContent = tag;
+    tagid.setAttribute('id', tag);
+    tagid.style.background = tag[1];
+    text.textContent = tag[0];
+    this.setAttribute('id', tag[0]);
+    this.style.margin = 1;
   }
 }
 
@@ -82,9 +74,10 @@ window.customElements.define('tag-bujo', tagBujo);
 document.addEventListener('DOMContentLoaded', () => {
   const tagholder = document.createElement('span');
   tagholder.id = 'tag-holder';
+
   document.body.appendChild(tagholder);
+  const summon = document.getElementById('tag-holder');
   for (const tag of sample) {
-    const summon = document.getElementById('tag-holder');
     const newTag = document.createElement('tag-bujo');
     newTag.tag = tag;
     summon.appendChild(newTag);
