bdc30d865b43fa98501f5a228d77a807d38653be
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2021-06-09 16:39:23 -0700

    Added goals-board editor and storage

--- project/src/components/journal-entry/journal-entry.js
+++ project/src/components/journal-entry/journal-entry.js
@@ -92,6 +92,7 @@ class JournalEntry extends HTMLElement {
         childList: true,
         subtree: true
       });
+
       function bulletFunctionality () {
         // Create event listener for each new bullet created
         const bullets = document.querySelector('journal-entry').shadowRoot.querySelectorAll('article > ul > li');
@@ -112,14 +113,12 @@ class JournalEntry extends HTMLElement {
             element.addEventListener('contextmenu', (e) => {
               hasListener.push(element);
               e.preventDefault();
-              console.log('right click');
               menu.style.top = `${e.clientY}px`;
               menu.style.left = `${e.clientX}px`;
               menu.classList.add('show');
               outClick.style.display = 'block';
 
               element.classList.add('selected');
-              console.log(element);
             });
           }
         });
@@ -132,7 +131,6 @@ class JournalEntry extends HTMLElement {
           const bullets = document.querySelector('journal-entry').shadowRoot.querySelectorAll('article > ul > li');
           bullets.forEach((element) => {
             if (element.classList.contains('selected')) {
-              console.log(element);
               element.className = event.target.value;
               element.classList.remove('selected');
             }
@@ -142,15 +140,143 @@ class JournalEntry extends HTMLElement {
             if (element.classList.contains('selected')) {
               element.classList.remove('selected');
             }
-          // menu.classList.remove('show');
           });
+
+          document.querySelector('journal-entry').shadowRoot.querySelector('.menu').style.visibility = 'hidden';
+          document.querySelector('journal-entry').saveEntry();
         });
       }
     }
 
-    window.setTimeout(() => {
+    setTimeout(() => {
       detectChanges();
     }, 500);
+  }
+
+  /**
+   * Gets called after the DOMContent gets loaded. For more information:
+   * https://stackoverflow.com/questions/54857905/deferred-setattribute-call-in-custom-element-constructor-causes-dom-error-is-it
+   */
+  connectedCallback () {
+    const backBtn = this.shadowRoot.querySelector('.entry-back-btn');
+    const forwardBtn = this.shadowRoot.querySelector('.entry-forward-btn');
+    this.setAttribute('date', (viewedDate.year + '-' + viewedDate.month + '-' + viewedDate.day));
+    /**
+     * Function that calls the debounce method to begin saving the entry to storage
+     * Code adapted from https://www.freecodecamp.org/news/javascript-debounce-example/
+     */
+    this.processChange = this.debounce(() => this.saveEntry());
+
+    // Eventlistener that will save the article after 500 ms of no inputs
+    this.shadowRoot.querySelector('article').addEventListener('input', () => {
+      console.log('Change content');
+      this.processChange();
+    });
+
+    // Eventlistener that will save the entry-title after 500 ms of no inputs
+    this.shadowRoot.querySelector('.entry-header').addEventListener('input', () => {
+      console.log('Change title');
+      this.processChange();
+    });
+
+    // Eventlistener that enables the back button to load the previous day's content (changes the viewedDate variable)
+    backBtn.addEventListener('click', () => {
+      updateViewedDate('Backward');
+      this.setAttribute('date', (viewedDate.year + '-' + viewedDate.month + '-' + viewedDate.day));
+
+      document.querySelector('goals-board').loadGoals();
+
+      this.validateEntry();
+      this.loadEntry();
+    });
+
+    // Eventlistener that enables the forward button to load the next day's content (changes the viewedDate variable)
+    forwardBtn.addEventListener('click', () => {
+      updateViewedDate('Forward');
+      this.setAttribute('date', (viewedDate.year + '-' + viewedDate.month + '-' + viewedDate.day));
+
+      document.querySelector('goals-board').loadGoals();
+
+      this.validateEntry();
+      this.loadEntry();
+    });
+
+    this.validateEntry();
+    this.loadEntry();
+  }
+
+  /**
+   * Checks to see if there is an object in the storage variable for this particular date
+   * If not it creates it.
+   */
+  validateEntry () {
+    if (!entryExists(viewedDate)) {
+      console.log('Adding date!');
+      const date = decodeDateInfoElement(this);
+      console.log(date);
+      createEntry(date);
+    }
+  }
+
+  /**
+   * Saves the current title, date, and content in the article element to localStorage.
+   */
+  saveEntry () {
+    const date = decodeDateInfoElement(this);
+    const entryTitle = this.shadowRoot.querySelector('.entry-title').innerText;
+    const entryDate = this.shadowRoot.querySelector('.entry-date').innerText;
+    const entryContent = this.shadowRoot.querySelector('article').innerHTML;
+    const viewedStorage = storage[date.year][date.month][date.day];
+
+    if (!entryExists(date)) {
+      createEntry(date);
+    }
+
+    viewedStorage.title = entryTitle;
+    viewedStorage.date = entryDate;
+    viewedStorage.content = entryContent;
+
+    saveStorage();
+  }
+
+  /**
+   * Loads the data from storage to the journal-entry. It loads the title, date, and
+   * content of the page. The content being any text or bullets.
+   */
+  loadEntry () {
+    const date = decodeDateInfoElement(this);
+    const entryTitle = this.shadowRoot.querySelector('.entry-title');
+    const entryDate = this.shadowRoot.querySelector('.entry-date');
+    const entryContent = this.shadowRoot.querySelector('article');
+    const viewedStorage = storage[date.year][date.month][date.day];
+
+    if (entryExists(date) && viewedStorage.title !== undefined) {
+      entryTitle.innerHTML = viewedStorage.title;
+      entryDate.innerHTML = viewedStorage.date;
+      entryContent.innerHTML = viewedStorage.content;
+    }
+  }
+
+  /**
+ * Method delays the call of the func by timeout amount of ms
+ * Code from https://www.freecodecamp.org/news/javascript-debounce-example/
+ * @param {function} func - callback function that should be run after timeout
+ * @param {number} timeout - number of ms to wait before running func
+ * @returns a function that starts a timre to call func.
+ */
+  debounce (func, timeout = 500) {
+    // Where the setTimeout is saved so that we can cancel the timer within timeout ms
+    let timer;
+    return (...args) => {
+      // Resets the timer
+      clearTimeout(timer);
+      /**
+       * Sets the timer to equal a timeout that will run after timeout ms.
+       * Uses the rest parameter (...args) to save all of the arguments into an array
+       * Passes those arguments into the callback function using apply
+       */
+      timer = setTimeout(() => { func.apply(this, args); }, timeout);
+    };
   }
 }
 
