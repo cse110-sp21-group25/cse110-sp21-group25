3f369a9b3380cb7da49064568bab8692fe8bc281
Jesse <jtwolf@ucsd.edu>
2021-06-09 22:35:04 -0700

    Added menu symbol tests into end-to-end.test.js

--- project/testing/end-to-end.test.js
+++ project/testing/end-to-end.test.js
@@ -252,8 +252,9 @@ describe('Testing Nav Bar Component', () => {
     expect(informationButton).toMatch(/cog-solid.svg/);
   });
 
-  // -------------------------------------------END OF TESTS FOR NAV-BAR COMPONENT-------------------------------------------------------------
+  // -------------------------------------------END OF TESTS FOR NAV-BAR COMPONENT---------------------------------------------------------------------
 
+  // -------------------------------------------BEGINNING OF TESTS FOR MOOD INDICATOR COMPONENT--------------------------------------------------------
   describe('Testing Mood Indicator Component', () => {
     beforeAll(async () => {
       await page.goto('http://127.0.0.1:8080');
@@ -390,4 +391,76 @@ describe('Testing Nav Bar Component', () => {
       expect(imageSrc).toMatch(/great-emo.png/);
     });
   });
+
+  // -------------------------------------------END OF TESTS FOR MOOD INDICATOR COMPONENT---------------------------------------------------------------------
+
+  // -------------------------------------------BEGINNING OF TESTS FOR MENU SYMBOLS COMPONENT-----------------------------------------------------------------
+
+  describe('Testing Menu Symbols Component', () => {
+    beforeAll(async () => {
+      await page.goto('http://127.0.0.1:8080');
+      /**
+       * Below is the line you can use if you want to test it against the current working prototype:
+       * await page.goto('https://cse110-sp21-group25.github.io/cse110-sp21-group25/project/src/index.html');
+       * For testing purposes though need to get the local copy working properly.
+       */
+      await page.waitForTimeout(500);
+    });
+
+    test('Test1: Validating that the Task Incomplete icon/symbol is correct', async () => {
+      const taskIncompleteIcon = await page.evaluate(() => {
+        const symbolMenu = document.querySelector('body > div > div.column.middle-column > menu-symbols');
+        return symbolMenu.shadowRoot.querySelector('#menu > table > tbody > tr:nth-child(1) > th:nth-child(1) > img').src;
+      });
+
+      expect(taskIncompleteIcon).toMatch(/task-incompl.svg/);
+    });
+
+    test('Test2: Validating that the Task Scheduled icon/symbol is correct', async () => {
+      const taskScheduledIcon = await page.evaluate(() => {
+        const symbolMenu = document.querySelector('body > div > div.column.middle-column > menu-symbols');
+        return symbolMenu.shadowRoot.querySelector('#menu > table > tbody > tr:nth-child(1) > th:nth-child(2) > img').src;
+      });
+
+      expect(taskScheduledIcon).toMatch(/task-scheduled.svg/);
+    });
+
+    test('Test3: Validating that the Inspiration icon/symbol is correct', async () => {
+      const inspirationIcon = await page.evaluate(() => {
+        const symbolMenu = document.querySelector('body > div > div.column.middle-column > menu-symbols');
+        return symbolMenu.shadowRoot.querySelector('#menu > table > tbody > tr:nth-child(1) > th:nth-child(3) > img').src;
+      });
+
+      expect(inspirationIcon).toMatch(/inspiration.svg/);
+    });
+
+    test('Test4: Validating that the Task Complete icon/symbol is correct', async () => {
+      const taskCompleteIcon = await page.evaluate(() => {
+        const symbolMenu = document.querySelector('body > div > div.column.middle-column > menu-symbols');
+        return symbolMenu.shadowRoot.querySelector('#menu > table > tbody > tr:nth-child(2) > th:nth-child(1) > img').src;
+      });
+
+      expect(taskCompleteIcon).toMatch(/task-compl.svg/);
+    });
+
+    test('Test5: Validating that the Event icon/symbol is correct', async () => {
+      const taskCompleteIcon = await page.evaluate(() => {
+        const symbolMenu = document.querySelector('body > div > div.column.middle-column > menu-symbols');
+        return symbolMenu.shadowRoot.querySelector('#menu > table > tbody > tr:nth-child(2) > th:nth-child(2) > img').src;
+      });
+
+      expect(taskCompleteIcon).toMatch(/event.svg/);
+    });
+
+    test('Test6: Validating that the Notes icon/symbol is correct', async () => {
+      const notesIcon = await page.evaluate(() => {
+        const symbolMenu = document.querySelector('body > div > div.column.middle-column > menu-symbols');
+        return symbolMenu.shadowRoot.querySelector('#menu > table > tbody > tr:nth-child(2) > th:nth-child(3) > img').src;
+      });
+
+      expect(notesIcon).toMatch(/notes.svg/);
+    });
+  });
+
+  // -------------------------------------------END OF TESTS FOR MENU SYMBOLS COMPONENT-----------------------------------------------------------------------
 });
